44516909d6d50bfb2c968da7f5a80631bea232c7
Dominik Burgdörfer <db@burgdoerfer.com>
2018-11-14 15:12:57 +0100

    Rename prototype.html to index.html

--- cypress/integration/Websh.js
+++ cypress/integration/Websh.js
@@ -2,10 +2,10 @@ describe("Websh", () => {
   it("Executes a sample script", () => {
     const script =
       "fixtures/echo?text=Foo | fixtures/sed?regex=Foo&replace=Bar | fixtures/cat";
-    cy.visit(
-      `http://localhost:5000/prototype?eval=${encodeURIComponent(script)}`
-    );
+
+    cy.visit(`http://localhost:5000?eval=${encodeURIComponent(script)}`);
     cy.wait(2000);
+
     cy.get("#commandWindow")
       .should(iframe => expect(iframe.contents().find("#text")).to.exist)
       .then(iframe => cy.wrap(iframe.contents().find("#text")))
