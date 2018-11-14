4ee20439a7e8f1dab91c66d8da75bf3ef0415041
Dominik Burgdörfer <db@burgdoerfer.com>
2018-11-14 15:45:11 +0100

    Refactor communication interface to pass arrays to programs

--- iframe/Runner.js
+++ iframe/Runner.js
@@ -1,14 +1,14 @@
 const runner = (payload, command) =>
-     new Promise(resolve => {
-        const iframe = document.getElementById("commandWindow");
-        window.onmessage = e => {
-            if (e.source !== iframe.contentWindow) return;
-            resolve(e.data);
-        };
-        iframe.src = command;
-        iframe.onload = () => {
-            iframe.contentWindow.postMessage(payload, "*");
-        };
-    });
+  new Promise(resolve => {
+    const iframe = document.getElementById("commandWindow");
+    window.onmessage = e => {
+      if (e.source !== iframe.contentWindow) return;
+      resolve(e.data);
+    };
+    iframe.src = command;
+    iframe.onload = () => {
+      iframe.contentWindow.postMessage([payload], "*");
+    };
+  });
 
-export default runner
+export default runner;
